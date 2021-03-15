ec6ecca0bb67919cdb3349b89a02a6bd303db38f
Vladi Ciuculescu <vladimir.ciuculescu@titeica.com>
2021-03-15 11:32:12 +0200

    Implemented Register and Login with firebase

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import React from "react";
+import { View, Text, StyleSheet,TextInput, TouchableOpacity  } from 'react-native';
+
+import * as firebase from "firebase";
+
+
+
 
 export default class LoginScreen extends React.Component{
+
+    state = {
+        email: "andrei@admin.com",
+        password: "awda",
+        errorMessage: null
+    };
+    
+
+    handleLogin = () => {
+        const { email, password } = this.state;
+
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email , password)
+            .catch(error => this.setState({ errorMessage: error.message }))
+    };
 
     render() {
         return (
@@ -10,22 +30,43 @@ export default class LoginScreen extends React.Component{
                 <Text style={styles.greeting}>{`Hello again. \n Welcome back.`}</Text>
                 
                 <View style = {styles.errorMessage}>
-                    <Text>Error</Text>
+                    {this.state.errorMessage && <Text style = {styles.error}>{this.state.errorMessage}</Text>}
                 </View>
 
                 <View style = {styles.form}>
                     <View>
                         <Text style={styles.inputTitle}>Email Address</Text>
-                        <TextInput style={styles.input} autoCapitalize="none"></TextInput>
+                        <TextInput
+                            style={styles.input}
+                            autoCapitalize="none"
+                            onChangeText={email => this.setState({ email })}
+                            value = {this.state.email}
+                        ></TextInput>
                     </View>
                 </View>
 
                  <View style = {styles.form}>
                     <View>
-                        <Text style={styles.inputTitle}>Password from the commit</Text>
-                        <TextInput style={styles.input} autoCapitalize="none" underlineColorAndroid="white"></TextInput>
+                        <Text style={styles.inputTitle}>Password</Text>
+                        <TextInput
+                            style={styles.input}
+                            secureTextEntry
+                            autoCapitalize="none"
+                            onChangeText={password => this.setState({ password })}
+                            value = {this.state.password}
+                        ></TextInput>
                     </View>
                 </View>
+
+                <TouchableOpacity style = {styles.button} onPress = {this.handleLogin}>
+                    <Text style = {{color:"#FFF", fontWeight:"500"}} >Sign in</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style = {{alignSelf:'center', marginTop:32}} onPress = {() => this.props.navigation.navigate("Register")}>
+                    <Text style = {{color: "#414959", fontSize:13}}>
+                         New to MedHelp ? <Text style = {{fontWeight:"500", color:"#E9446A" }}>Sign Up </Text>
+                    </Text>
+                </TouchableOpacity>
 
             </View>
             
@@ -48,8 +89,17 @@ const styles = StyleSheet.create({
         marginTop:30,
         alignItems: "center",
         justifyContent: "center",
+        marginLeft: 25,
+        marginRight:25,
         
     },
+    error: {
+        color: "#E9446A",
+        fontSize: 13,
+        fontWeight: "600",
+        textAlign:'center',
+    },
+
     form: {
         marginBottom: 48,
         marginHorizontal: 30,
@@ -66,6 +116,14 @@ const styles = StyleSheet.create({
         fontSize: 15,
         color: "black",
         
+    },
+    button: {
+        marginHorizontal: 30,
+        backgroundColor: "#E9446A",
+        borderRadius: 4,
+        height: 52,
+        justifyContent: 'center',
+        alignItems:'center',
     }
 
 })
